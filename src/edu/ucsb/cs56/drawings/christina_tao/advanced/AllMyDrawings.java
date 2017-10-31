c82890715e0bc0b3081d983d2a8e3302ab06769c
christinatao31 <christinatao31@gmail.com>
2017-10-30 19:39:50 -0700

    wrote ChristmasTree class and added drawPicture methods in AllMyDrawings

--- src/edu/ucsb/cs56/drawings/christina_tao/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/drawings/christina_tao/advanced/AllMyDrawings.java
@@ -25,13 +25,13 @@ public class AllMyDrawings
         g2.setColor(Color.BLACK);
         g2.draw(t1);
 
-        Shape t2 = ShapeTransforms.scaledCopyOfLL(t1,2,3);
-        t2 = ShapeTransforms.translatedCopyOf(t2,350,200);
+        Shape t2 = ShapeTransforms.scaledCopyOf(t1,2,3);
+        t2 = ShapeTransforms.translatedCopyOf(t2,330,25);
         g2.setColor(Color.RED);
         g2.draw(t2);
 
-        Shape t3 = ShapeTransforms.scaledCopyOfLL(t1,3,2.5);
-        t3 = ShapeTransforms.translatedCopyOf(t3, 20, 220);
+        Shape t3 = ShapeTransforms.scaledCopyOf(t1,3,2.5);
+        t3 = ShapeTransforms.translatedCopyOf(t3, 20, 100);
         t3 = ShapeTransforms.rotatedCopyOf(t3, -Math.PI/3);
         g2.setColor(Color.GREEN);
         g2.draw(t3);
@@ -43,13 +43,62 @@ public class AllMyDrawings
     
     public static void drawPicture2(Graphics2D g2) {
 
-	
+	   ChristmasTree c1 = new ChristmasTree(50,50,150,150);
+       g2.setColor(Color.BLACK);
+       g2.draw(c1);
+
+       Shape c2 = ShapeTransforms.scaledCopyOf(c1,0.6,0.75);
+       c2 = ShapeTransforms.translatedCopyOf(c2,75, 220);
+       g2.setColor(Color.RED);
+       g2.draw(c2);
+
+       Shape c3 = ShapeTransforms.scaledCopyOf(c1,1.7,1.4);
+       c3 = ShapeTransforms.translatedCopyOf(c3, 275,-10);
+       c3 = ShapeTransforms.rotatedCopyOf(c3, Math.PI / 3);
+       g2.setColor(Color.GREEN);
+       g2.draw(c3);
+
+       g2.setColor(Color.BLACK);
+       g2.drawString("A few Christmas trees by Christina Tao", 10,20);
 
     }
   
     public static void drawPicture3(Graphics2D g2) {
 
-	
+	   Tree t1 = new Tree(50,50,50,50);
+       g2.setColor(Color.BLACK);
+       g2.draw(t1);
+
+       Shape t2 = ShapeTransforms.scaledCopyOf(t1,2.2,2.7);
+       t2 = ShapeTransforms.translatedCopyOf(t2,350,20);
+       t2 = ShapeTransforms.rotatedCopyOf(t2, Math.PI);
+       g2.setColor(Color.RED);
+       g2.draw(t2);
+
+       Shape t3 = ShapeTransforms.scaledCopyOf(t1,3.2,1.8);
+       t3 = ShapeTransforms.translatedCopyOf(t3,100,-10);
+       t3 = ShapeTransforms.rotatedCopyOf(t3, Math.toRadians(290));
+       g2.setColor(Color.GREEN);
+       g2.draw(t3);
+
+       ChristmasTree c1 = new ChristmasTree(300,150,140,180);
+       g2.setColor(Color.BLACK);
+       g2.draw(c1);
+
+       Shape c2 = ShapeTransforms.scaledCopyOf(c1,1.9,1.3);
+       c2 = ShapeTransforms.translatedCopyOf(c2,-300,-20);
+       c2 = ShapeTransforms.rotatedCopyOf(c2, Math.toRadians(210));
+       g2.setColor(Color.RED);
+       g2.draw(c2);
+
+       Shape c3 = ShapeTransforms.scaledCopyOf(c1,0.7,0.9);
+       c3 = ShapeTransforms.translatedCopyOf(c3,150,70);
+       c3 = ShapeTransforms.rotatedCopyOf(c3, Math.PI / 7);
+       g2.setColor(Color.GREEN);
+       g2.draw(c3);
+
+       g2.setColor(Color.BLACK);
+       g2.drawString("A few regular trees and a few Christmas trees by Christina Tao", 10,20);
 
     }       
 }
--- src/edu/ucsb/cs56/drawings/christina_tao/advanced/Tree.java
+++ src/edu/ucsb/cs56/drawings/christina_tao/advanced/Tree.java
@@ -7,13 +7,32 @@ import java.awt.geom.Rectangle2D;
 import edu.ucsb.cs56.drawings.utilities.ShapeTransforms;
 import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
 
+/**
+ * A vector drawing of a tree that implements
+ * the Shape interface, and so can be drawn, as well as
+ * rotated, scaled, etc.
+ *
+ * @author Christina Tao
+ * @version for CS56, F17, UCSB
+ */
+
 public class Tree extends GeneralPathWrapper implements Shape {
+
+	/**
+	 * Constructor for objects of class Tree
+	 *
+	 * @param x x coord of lower left corner of tree
+	 * @param y y coord of lower left corner of tree
+	 * @param width width of the tree
+	 * @param height height of the tree
+	 */
 
 	public Tree(double x, double y, double width, double height) {
 
 		final double ORIG_ULX = x + 0.5 * width;
 		final double ORIG_ULY = y;
 
+		// make the left side of the tree
 		GeneralPath leftSide = new GeneralPath();
 
 		leftSide.moveTo(ORIG_ULX, ORIG_ULY);
@@ -24,18 +43,23 @@ public class Tree extends GeneralPathWrapper implements Shape {
 		leftSide.lineTo(ORIG_ULX - 0.5 * width, ORIG_ULY + 0.93 * height);
 		leftSide.lineTo(ORIG_ULX - 0.375 * width, ORIG_ULY + 0.93 * height);
 
+		// make the right side of the tree by horizontally
+		// flipping the left side, and translating accordingly
 		Shape rightSide = ShapeTransforms.horizontallyFlippedCopyOf(leftSide);
 		rightSide = ShapeTransforms.translatedCopyOf(rightSide, width, 0);
 
+		// make the bottom of the tree
 		GeneralPath bottom = new GeneralPath();
 
 		bottom.moveTo(ORIG_ULX - 0.375 * width, ORIG_ULY + 0.93 * height);
 		bottom.lineTo(ORIG_ULX + 0.375 * width, ORIG_ULY + 0.93 * height);
 
+		// make the tree trunk
 		Rectangle2D.Double trunk = 
 		new Rectangle2D.Double(ORIG_ULX - 0.05 * width, ORIG_ULY + 0.93 * height,
-								0.1 * width, 0.07 * height);
+			0.1 * width, 0.07 * height);
 
+		// put the whole tree together
 		GeneralPath wholeTree = this.get();
 		wholeTree.append(leftSide, false);
 		wholeTree.append(rightSide, false);
